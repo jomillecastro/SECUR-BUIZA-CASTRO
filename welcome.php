b41f74cbbdc6962378f776770c93074a718a27ed
Jomille Anne Castro <jomillecastro@users.noreply.github.com>
2015-10-09 19:49:25 +0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,27 @@
+<body background = "abstract-white-desktop-backgrounds..jpg" height=100% width=100% bgproperties = "fixed">
+<p><a href=logout.php><img src = "lo.png"></a>
+<?php
+
+	if(isset($_POST['login']))
+			echo "<h2><b>Welcome ".$_POST['first'];
+			echo "<br>";
+			echo "<br>";
+			echo "<b>Today is "; echo date("<b>F d, Y </b></h2>");
+
+		
+    
+	
+	?>
+<br>
+
+	<center>
+	<img src="10689698_866326633401266_5049155240085410377_n.jpg" alt= "page1.html" width="600" height="250"><br>
+	<img src= "cooltext1881508275.png" width=450 height=50><br><br><br>
+<a href="page1.html"><img src="bn.gif" alt= "page1.html" width="300" height="100"></a>
+
+<br>
+<br>
+<br>
+</body>
+</center>
+<font align =left><h3><b> &copy;copyright by: Buiza, M. & Castro, J. </font></b></h3>
