e93bf0ab0e22dc7120ba45c70f4c2cae99b76799
Jomille Anne Castro <jomillecastro@users.noreply.github.com>
2015-10-09 19:51:03 +0800

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,23 @@
+<body background=10492501_796854237015173_4365597652353058609_n.jpg bgproperties=fixed >
+<center>
+<img src="10689698_866326633401266_5049155240085410377_n.jpg" alt= "page1.html" width="450" height="100"><br>
+<img src= "cooltext1881508275.png" width=450 height=50><br><br><br>
+<img src= "cooltext1881981067.png" width=550 height=90><br>
+<form action=summary.php method=get>
+<table border=1 cellspacing="1" cellpadding="1" bgcolor=white >
+<tr><td colspan=1><font color=red><b><center>CONFIRMATION OF ORDER</b></center></font>
+<tr><td><b>Item Name:
+<input name=item value="<?php print $_GET['item'];?>">
+<tr><td><b>Item Price: 
+<input name=ucost value="<?php print $_GET['ucost'];?>">
+<tr><td><b><font color=red>Quantity:</font>
+<input type=text name=qty required>
+<tr><td><a href = "summary.php"> <input type=submit value='Confirm Order' name=order> </a>
+
+</table>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+</center>
+<font align =left><h3><b> &copy;copyright by: Buiza, M. & Castro, J.</font></b></h3>
